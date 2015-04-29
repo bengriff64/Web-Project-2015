c94f6a9933ff63a7cbd881bca8e2cf432ba217ed
Adam Botley <adambotley@gmail.com>
2015-04-29 21:35:25 +0100

    Sidebar Position Fixing
    Added sidebar class instantiation and fixing based on height of navber
    + dashboard.
    
    Sidebar will now fix to top of screen when below the Dashboard

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,51 @@
+var $window = $(window);
+var sidebar = null;
+
 $(document).ready(function () {
-  console.log('ready');
+	console.log('ready');
+	setEventListeners();
+
+	// NOTE: Sidebar height is 0 when display: none
+	var sidebarFixPos = $('#navbar').height() + $('#dashboardContainer').height() - 100;
+	sidebar = new Sidebar({
+		fixPos: sidebarFixPos
+	});
+	sidebar.unfix();
 });
 
 var setEventListeners = function () {
-  $('#toggleSidebar').click(function (e) {
-    $('#sidebar').toggle('show');
-  });
+	$('#toggleSidebar').click(function (e) {
+		$('#sidebar').slideToggle();
+	});
+
+	$window.scroll(function (e) {
+		if ($window.scrollTop() < sidebar.fixPos) {
+			sidebar.unfix();
+		}
+		else {
+			sidebar.fix();
+		}
+	});
 };
 
-var Dashboard = function () {
+var Sidebar = function (params) {
 
+	var $sidebar = $('#sidebar');
+	var _self = this;
+
+	// Height at which the sidebar fixes to top of screen
+	this.fixPos = params.fixPos || 0;
+
+	this.unfix = function () {
+		$sidebar.css({
+			position: 'absolute',
+			top: _self.fixPos
+		});
+	};
+	this.fix = function () {
+		$sidebar.css({
+			position: 'fixed',
+			top: 0
+		});
+	};
 };
